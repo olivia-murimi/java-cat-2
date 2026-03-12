4b27a37cdfc7bd7ad357d5ef87dd9136e3c8389a
Olivia Waithira <oliviawaithira7@gmail.com>
2026-03-12 21:13:36 +0300

    studentapp

--- StudentApp.java
+++ StudentApp.java
@@ -1,4 +1,24 @@
 import java.util.Scanner;
+
 public class StudentApp {
-    
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Student ID: ");
+        String id = input.nextLine();
+
+        System.out.print("Enter Student Name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter Course: ");
+        String course = input.nextLine();
+
+        StudentRecord student = new StudentRecord(id, name, course);
+
+        student.displayInfo();
+
+        input.close();
+    }
 }
